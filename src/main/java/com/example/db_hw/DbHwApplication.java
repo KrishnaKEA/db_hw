a1beecdfa20b4dc852239ab8707d603cb5d16c28
KrishnaKEA <82447207+KrishnaKEA@users.noreply.github.com>
2021-10-01 11:03:33 +0200

    Changes

---
+++ src/main/java/com/example/db_hw/DbHwApplication.java
@@ -0,0 +1,20 @@
+package com.example.db_hw;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbHwApplication {
+    public static String ip;
+   public static String pass;
+
+    public static void main(String[] args) {
+
+        ip = args[0];
+        pass = args[1];
+
+        SpringApplication.run(DbHwApplication.class, args);
+    }
+
+}
